a978a2b5cf12cdf5a047ef93e4b276e05c73e8b4
SHIKHAR DEEP <deepshikhar24@yahoo.com>
2017-04-12 16:27:00 +0530

    initial commit. added libraries

---
+++ app/src/main/java/com/shikhar/marvelpedia/Activity/CharDetailsActivity.java
@@ -0,0 +1,15 @@
+package com.shikhar.marvelpedia.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.shikhar.marvelpedia.R;
+
+public class CharDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_char_details);
+    }
+}
---
+++ app/src/main/java/com/shikhar/marvelpedia/Activity/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.shikhar.marvelpedia.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.shikhar.marvelpedia.BuildConfig;
+import com.shikhar.marvelpedia.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final String API_KEY = BuildConfig.API_KEY;
+
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        tv = (TextView)findViewById(R.id.text);
+        tv.setText(API_KEY);
+    }
+}
+
+//TODO
+//try api key as initial commit on github
+
+//splash activity, add photos no internet(image), no data found(when wrong text entered for search na)(image)         network_error(toast)
+// picasso - error,placeholder(diff images)
+
+//see glide notepad
+
+//see retorofit me 100 limit dalne se data aa raha hai na??othewise 90,80 ....
