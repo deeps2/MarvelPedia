5b6bff8c152138ce02a601250b413f136cb03b70
SHIKHAR DEEP <deepshikhar24@yahoo.com>
2017-04-12 17:29:34 +0530

    testing: hiding api key feature

--- app/src/main/java/com/shikhar/marvelpedia/Activity/SplashActivity.java
+++ app/src/main/java/com/shikhar/marvelpedia/Activity/SplashActivity.java
@@ -7,9 +7,15 @@ import android.widget.TextView;
 import com.shikhar.marvelpedia.BuildConfig;
 import com.shikhar.marvelpedia.R;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import static android.R.attr.password;
+
 public class SplashActivity extends AppCompatActivity {
 
-    private static final String API_KEY = BuildConfig.API_KEY;
+    private static String API_KEY = BuildConfig.API_KEY;
 
     TextView tv;
 
@@ -20,11 +26,11 @@ public class SplashActivity extends AppCompatActivity {
 
         tv = (TextView)findViewById(R.id.key);
         tv.setText(API_KEY);
+
     }
 }
 
 //TODO
-//try api key as initial commit on github
 
 //splash activity, add photos no internet(image), no data found(when wrong text entered for search na)(image)         network_error(toast)
 // picasso - error,placeholder(diff images)
